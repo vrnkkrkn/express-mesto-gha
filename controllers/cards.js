9d26b814d4cb6609ad7f71c06da3fbf01ad0356f
Veronika Karkan <PapaGoose000@yandex.ru>
2023-10-01 20:51:22 +0300

    2

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-underscore-dangle */
 const Card = require('../models/card');
 
 /** создать карточку */
@@ -7,7 +6,7 @@ module.exports.createCard = (req, res) => {
   const owner = req.user._id;
   Card
     .create({ name, link, owner })
-    .then((card) => res.send({ card }))
+    .then((card) => res.status(201).res.send({ card }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
         res.status(400).send({ message: 'Переданы некорректные данные' });
@@ -27,54 +26,57 @@ module.exports.getCard = (req, res) => {
 
 /** удаление карточки */
 module.exports.deleteCard = (req, res) => {
-  if (req.params.cardId.length === 24) {
-    Card
-      .findByIdAndRemove(req.params.cardId)
-      // eslint-disable-next-line consistent-return
-      .then((card) => {
-        if (!card) {
-          return res.status(404).send({ message: 'Карточка не найдена' });
-        }
-        res.send({ message: 'Карточка удалена' });
-      })
-      .catch(() => res.status(404).send({ message: 'Карточка не найдена' }));
-  } else {
-    res.status(400).send({ message: 'Переданы некорректные данные' });
-  }
+  Card
+    .findByIdAndRemove(req.params.cardId)
+    .then((card) => {
+      if (!card) {
+        return res.status(404).send({ message: 'Карточка не найдена' });
+      }
+      return res.send({ message: 'Карточка удалена' });
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(400).send({ message: 'Переданы некорректные данные' });
+      } else {
+        res.status(500).send({ message: 'Произошла ошибка' });
+      }
+    });
 };
 
 /** лайк */
 module.exports.likeCard = (req, res) => {
-  if (req.params.cardId.length === 24) {
-    Card
-      .findByIdAndUpdate(req.params.cardId, { $addToSet: { likes: req.user._id } }, { new: true })
-      // eslint-disable-next-line consistent-return
-      .then((card) => {
-        if (!card) {
-          return res.status(404).send({ message: 'Карточка не найдена' });
-        }
-        res.send({ card, message: 'Лайк поставлен' });
-      })
-      .catch(() => res.status(404).send({ message: 'Карточка не найдена' }));
-  } else {
-    res.status(400).send({ message: 'Переданы некорректные данные' });
-  }
+  Card
+    .findByIdAndUpdate(req.params.cardId, { $addToSet: { likes: req.user._id } }, { new: true })
+    .then((card) => {
+      if (!card) {
+        return res.status(404).send({ message: 'Карточка не найдена' });
+      }
+      return res.send({ card, message: 'Лайк поставлен' });
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(400).send({ message: 'Переданы некорректные данные' });
+      } else {
+        res.status(500).send({ message: 'Произошла ошибка' });
+      }
+    });
 };
 
 /** убрать лайк */
 module.exports.dislikeCard = (req, res) => {
-  if (req.params.cardId.length === 24) {
-    Card
-      .findByIdAndUpdate(req.params.cardId, { $pull: { likes: req.user._id } }, { new: true })
-      // eslint-disable-next-line consistent-return
-      .then((card) => {
-        if (!card) {
-          return res.status(404).send({ message: 'Карточка не найдена' });
-        }
-        res.send({ card, message: 'Лайк удален' });
-      })
-      .catch(() => res.status(404).send({ message: 'Карточка не найдена' }));
-  } else {
-    res.status(400).send({ message: 'Переданы некорректные данные' });
-  }
+  Card
+    .findByIdAndUpdate(req.params.cardId, { $pull: { likes: req.user._id } }, { new: true })
+    .then((card) => {
+      if (!card) {
+        return res.status(404).send({ message: 'Карточка не найдена' });
+      }
+      return res.send({ card, message: 'Лайк удален' });
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(400).send({ message: 'Переданы некорректные данные' });
+      } else {
+        res.status(500).send({ message: 'Произошла ошибка' });
+      }
+    });
 };
--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-underscore-dangle */
 const User = require('../models/user');
 
 /** создать пользователя */
@@ -6,7 +5,7 @@ module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User
     .create({ name, about, avatar })
-    .then((user) => res.send({
+    .then((user) => res.status(201).res.send({
       name: user.name, about: user.about, avatar: user.avatar, _id: user._id,
     }))
     .catch((err) => {
@@ -28,64 +27,60 @@ module.exports.getUser = (req, res) => {
 
 /** возвращает пользователя по _id */
 module.exports.getUserId = (req, res) => {
-  if (req.params.userId.length === 24) {
-    User
-      .findById(req.params.userId)
-      // eslint-disable-next-line consistent-return
-      .then((user) => {
-        if (!user) {
-          res.status(404).send({ message: 'Пользователь не найден' });
-          return;
-        }
-        res.send(user);
-      })
-      .catch(() => res.status(404).send({ message: 'Пользователь не найден' }));
-  } else {
-    res.status(400).send({ message: 'Переданы некорректные данные' });
-  }
+  User
+    .findById(req.params.userId)
+    .then((user) => {
+      if (!user) {
+        res.status(404).send({ message: 'Пользователь не найден' });
+        return;
+      }
+      res.send(user);
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(400).send({ message: 'Переданы некорректные данные' });
+      } else {
+        res.status(500).send({ message: 'Произошла ошибка' });
+      }
+    });
 };
 
 /** обновить профиль */
 module.exports.updateProfile = (req, res) => {
   const { name, about } = req.body;
-  // eslint-disable-next-line no-underscore-dangle
   User
-    // eslint-disable-next-line no-underscore-dangle
     .findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
-    // eslint-disable-next-line consistent-return
     .then((user) => {
       if (!user) {
         return res.status(404).send({ message: 'Пользователь не найден' });
       }
-      res.send(user);
+      return res.send(user);
     })
     .catch((err) => {
       if (err.name === 'ValidationError') {
         res.status(400).send({ message: 'Переданы некорректные данные' });
       } else {
-        res.status(404).send({ message: 'Пользователь не найден' });
+        res.status(500).send({ message: 'Произошла ошибка' });
       }
     });
 };
+
 /** обновить аватар */
 module.exports.updateAvatar = (req, res) => {
   const { avatar } = req.body;
-  // eslint-disable-next-line no-underscore-dangle
   User
-    // eslint-disable-next-line no-underscore-dangle
     .findByIdAndUpdate(req.user._id, { avatar }, { new: true })
-    // eslint-disable-next-line consistent-return
     .then((user) => {
       if (!user) {
         return res.status(404).send({ message: 'Пользователь не найден' });
       }
-      res.send(user);
+      return res.send(user);
     })
     .catch((err) => {
       if (err.name === 'ValidationError') {
         res.status(400).send({ message: 'Переданы некорректные данные' });
       } else {
-        res.status(404).send({ message: 'Пользователь не найден' });
+        res.status(500).send({ message: 'Произошла ошибка' });
       }
     });
 };
